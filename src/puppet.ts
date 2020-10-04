2f0df6b5f76692a3592c71a5e26831aa8bc04739
magnussony <magnuslundstromdk@gmail.com>
2020-10-04 22:06:31 +0200

    trying agane

--- src/puppet.ts
+++ src/puppet.ts
@@ -16,7 +16,6 @@ export const startLiking = async ({
 }: Body): Promise<Result> => {
   try {
     const browser = await puppeteer.launch({
-      headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
